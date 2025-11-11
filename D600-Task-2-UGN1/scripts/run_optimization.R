c853321fbffdbf7de3ba9221da4a71750973e4f7
verneshia-cloud <verneshiasmith3@gmail.com>
2025-11-11 10:25:57 -0600

    Delete D600-Task-2-UGN1/scripts/run_optimization.R

--- D600-Task-2-UGN1/scripts/run_optimization.R
+++
@@ -1,7 +0,0 @@
-# run_optimization.R
-# Helper to run the D2 optimization end-to-end
-
-message("Running scripts/02_optimize_model.R ...")
-source("scripts/02_optimize_model.R")
-message("Done. Artifacts written to ./outputs:")
-print(list.files("outputs", full.names = TRUE))
